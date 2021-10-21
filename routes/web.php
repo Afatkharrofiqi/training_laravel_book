4c190199087a51ed60c0d70c913b1ace13a3bd98
Achmad Fatkharrofiqi <achmad@brainmatics.com>
2021-10-21 12:36:01 +0700

    [ADD] ada perubahan di routes

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
+// ini data baru
 Route::middleware('auth')->group(function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
